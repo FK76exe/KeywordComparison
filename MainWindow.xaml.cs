674d3c6cb0a6318b983149dd491b6fe6367ad196
FK76exe <xerevofaris76@gmail.com>
2026-03-17 13:08:01 -0400

    Added sample functions for MainWindow.cs, scoring function and display, and a README

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,35 @@ namespace KeywordComparison
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CompareText(object sender, RoutedEventArgs e)
+        {
+            int jobDescCount = keywordApperance(Keywords, txtBox1);
+            int resumeCount = keywordApperance(Keywords, txtBox2);
+            
+            if (jobDescCount == 0 )
+            {
+                scoreDisplay.Content = "Score: 0%";
+            }
+            else
+            {
+                Double score =  ((Double) resumeCount / jobDescCount) * 100;
+                scoreDisplay.Content = $"Score: {score.ToString("F0")}%";
+            }
+        }
+        private int keywordApperance(ListBox keywords, TextBox body)
+        {
+            int keywordCount = 0;
+            for (int i = 0; i < keywords.Items.Count; i++)
+            {
+                ListBoxItem item = (ListBoxItem) keywords.Items.GetItemAt(i);
+                String keyword = (String) item.Content;
+                if (body.Text.Contains(keyword)) {
+                    keywordCount++;
+                }
+            }
+            return keywordCount;
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,25 @@
+﻿# Keyword Comparison App
+
+This application takes in a job description, a plain text resume, and a list of keywords to search for.
+Upon pressing "Submit", the user's resume receives a score based on this formula:
+
+```
+(number of unique keywords in resume) / (number of unique keywords in job description) as a %
+```
+
+A high % means that the resume shares a lot of keywords with the job description, making a good match.
+Otherwise, it's a low match.
+
+This application was made with C# and Windows Presentation Foundation (WPF) as a learning tool.
+
+```
+        /* TODO (2026-03-17)
+         * - get keywords from job desc
+         * - fill that into keyword apperance with body
+         * - match full words
+         * - fix variable names
+         * - display missing keywords
+         * - add/remove keywords
+         */
+
+```
